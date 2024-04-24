28a5fb8b8a4727e117b2954ed586700925c14347
chapter1617 <goyal.mansi16@gmail.com>
2024-04-24 15:52:32 -0700

    final changes

--- AirBnb-react-project/src/App.jsx
+++ AirBnb-react-project/src/App.jsx
@@ -4,13 +4,15 @@ import Hero from './components/Hero'
 import Card from './components/Card'
 import data from './data'
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
+
 function App() {
  
   const cards = data.map(item => {
     return (
         <Card 
             key={item.id}
-            item={item}
+            {...item}
         />
     )
   })     
