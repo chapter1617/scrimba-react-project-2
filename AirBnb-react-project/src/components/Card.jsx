7c439e0934a1e80e7bf58b894bea8b049280056c
chapter1617 <goyal.mansi16@gmail.com>
2024-04-23 17:20:50 -0700

    airbnb objects as props

--- AirBnb-react-project/src/components/Card.jsx
+++ AirBnb-react-project/src/components/Card.jsx
@@ -1,24 +1,28 @@
 export default function Card(props){
 
     let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     }
-
-    return(
+    
+    /*
+    Challenge: Fix our component! 😱
+    */
+    
+    return (
         <div className="card">
             {badgeText && <div className="card--badge">{badgeText}</div>}
-            <img src={`./Images/${props.img}`} className="card--image" alt="Main card image."/>
+            <img src={`./Images/${props.item.coverImg}`} className="card--image" />
             <div className="card--stats">
-                <img src="./Images/star.png" className="card--star" alt="Star icon."/>
-                <span>{props.rating}</span>
-                <span className="gray">({props.reviewCount})  • </span>
-                <span className="gray">{props.location}</span>
+                <img src="./Images/star.png" className="card--star" />
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) • </span>
+                <span className="gray">{props.item.location}</span>
             </div>
-            <h2>{props.title}</h2>
-            <p><span className="bold">From ${props.price}</span> / person</p>
+            <p className="card--title">{props.item.title}</p>
+            <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
         </div>
     )
 }
