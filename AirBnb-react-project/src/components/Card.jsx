3b2875d85f83eb377375bc8370daeb4d12d332df
chapter1617 <goyal.mansi16@gmail.com>
2024-04-23 17:13:20 -0700

    airbnb card badge

--- AirBnb-react-project/src/components/Card.jsx
+++ AirBnb-react-project/src/components/Card.jsx
@@ -1,6 +1,15 @@
 export default function Card(props){
+
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
     return(
         <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={`./Images/${props.img}`} className="card--image" alt="Main card image."/>
             <div className="card--stats">
                 <img src="./Images/star.png" className="card--star" alt="Star icon."/>
